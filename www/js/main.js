8233c33b5f547fbf82e3c7f5cd5188055b550ace
xereniak <xereniak@gmail.com>
2019-03-19 09:46:26 -0600

    moving to school computers

--- www/js/main.js
+++ www/js/main.js
@@ -18,7 +18,11 @@ init()
 
 function init() {
     //    Vars
-    var i, coords;
+    var i, coords, watchID, map, marker, typeTrap;
+
+    var geoOpts = {
+        enableHighAccuracy: true
+    }
 
     // Arrays
     var markerCalls = [
@@ -26,7 +30,8 @@ function init() {
             position: {
                 lat: 49.6776404,
                 lng: -112.8593567
-            }
+            },
+            markerType: "help"
         },
         {
             position: {
@@ -39,28 +44,50 @@ function init() {
                 lat: 49.677742,
                 lng: -112.863450
             }
+        },
+        {
+            position: {
+                lat: 49.5025,
+                lng: -112.5217
+            }
         }
     ]
-
+    var markerType = [
+        "help",
+        "bomb",
+        "help",
+        "bomb"
+    ]
     // Start Geolocation Scripts
-    var geoOpts = {
-        enableHighAccuracy: true
+
+
+    function watchSuccess(position) {
+        coords = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        markerself.setPosition(coords)
+        map.setCenter(coords)
     }
 
-    var watchID, map, marker;
+
+    function geoLose(m) {
+        //        console.log(`Uh oh! We made a little oopsie whoopsie! UwU  ${m}`)
+    }
 
 
     function geoWin(position) {
-        console.log("yes");
         coords = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
         //        Initiation of Google Map API and options
         map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
+            zoom: 18,
             center: coords,
-            disableDefaultUI: true
+            disableDefaultUI: true,
+            clickableIcons: false,
+            gestureHandling: "none"
         })
 
         function markers() {
@@ -81,27 +108,27 @@ function init() {
         }
 
         markers()
+        triangulate(position)
 
         navigator.geolocation.watchPosition(watchSuccess, geoLose, geoOpts)
-
-        function watchSuccess(position) {
-            coords = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            markerself.setPosition(coords)
-            map.setCenter(coords)
-        }
-        console.log("x = " + position.coords.latitude)
-        console.log("y = " + position.coords.longitude)
-    }
-
-
-
-    function geoLose(m) {
-        //        console.log(`Uh oh! We made a little oopsie whoopsie! UwU  ${m}`)
     }
 
     navigator.geolocation.getCurrentPosition(geoWin, geoLose, geoOpts);
 
+    function triangulate(position) {
+        console.log("I am here")
+        for (i = 0; i < markerCalls.length; i++) {
+            var A = markerCalls[i].position.lat - position.coords.latitude;
+            var B = markerCalls[i].position.lng - position.coords.longitude;
+            var C2 = A * A + B * B;
+            var C = Math.sqrt(C2)
+            var lats = Math.sqrt(position.coords.latitude * position.coords.latitude + markerCalls[i].position.lat * markerCalls[i].position.lat)
+            console.log(C)
+
+            if (C < 0.0004) {
+                typeTrap = i
+                return typeTrap
+            }
+        }
+    }
 }
