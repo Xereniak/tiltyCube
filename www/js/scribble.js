3619f4da6739d0c20898f86fe42eb970771d063b
xereniak <xereniak@gmail.com>
2019-04-05 16:54:41 -0600

    The whole thing basically

--- www/js/scribble.js
+++ www/js/scribble.js
@@ -25,7 +25,7 @@ function handleOrientation(e) {
     engine.world.gravity.x = yGrav
     //    engine.world.gravity.y = -1
 
-    $(".floating-integers").html("<p>World Gravity (Y): " + engine.world.gravity.y + "</p><p>X (Gravity): " + xGrav + "</p><p>Y (Gravity): " + yGrav + "</p>")
+    //    $(".floating-integers").html("<p>World Gravity (Y): " + engine.world.gravity.y + "</p><p>X (Gravity): " + xGrav + "</p><p>Y (Gravity): " + yGrav + "</p>")
 }
 
 
@@ -52,6 +52,10 @@ var boxA;
 var boxB;
 var boxC;
 var barrierT, barrierR, barrierB, barrierL;
+//Initializing vars for internal barriers
+//var wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11;
+//var walls = [wall1, wall2, wall3, wall4, wall5]
+var walls = []
 
 function setup() {
     var cnv = createCanvas(window.innerWidth, window.innerHeight)
@@ -61,24 +65,61 @@ function setup() {
     engine = Engine.create();
 
     // create two boxes and a ground
-    boxA = Bodies.rectangle(width / 2, 200, 80, 80);
-    boxB = Bodies.rectangle(width / 2, 50, 80, 80);
-    boxC = Bodies.rectangle(25, 25, 30, 30);
+    //    boxA = Bodies.rectangle(width / 2, 200, 80, 80);
+    //    boxB = Bodies.rectangle(width / 2, 50, 80, 80);
+    boxC = Bodies.rectangle(width * .05, height * .05, width * .08, width * .08);
     barrierB = Bodies.rectangle(width / 2, height, width, height / 21, {
         isStatic: true
     });
     barrierT = Bodies.rectangle(width / 2, 0, width, height / 21, {
         isStatic: true
     });
-    barrierR = Bodies.rectangle(width, height / 2, width / 15, height, {
+    barrierR = Bodies.rectangle(width, height / 2, height / 21, height, {
         isStatic: true
     });
-    barrierL = Bodies.rectangle(0, height / 2, width / 15, height, {
+    barrierL = Bodies.rectangle(0, height / 2, height / 21, height, {
         isStatic: true
     });
 
     // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, boxC, barrierB, barrierT, barrierR, barrierL]);
+    World.add(engine.world, [boxC, barrierB, barrierT, barrierR, barrierL]);
+
+    //setting up internal barriers
+    walls[0] = Bodies.rectangle(width * .3, height * .15, width * .9, height * .05, {
+        isStatic: true
+    });
+    walls[1] = Bodies.rectangle(width * .7, height * .3, width * .9, height * .05, {
+        isStatic: true
+    });
+    walls[2] = Bodies.rectangle(width * .3, height * .43, width * .9, height * .05, {
+        isStatic: true
+    });
+    walls[3] = Bodies.rectangle(width * .7, height * .7, width * .9, height * .05, {
+        isStatic: true
+    });
+    walls[4] = Bodies.rectangle(width * .25, height * .625, height * .05, height * .2, {
+        isStatic: true
+    });
+    walls[5] = Bodies.rectangle(width * .15, height * .925, width * .025, height * .15, {
+        isStatic: true
+    });
+    walls[6] = Bodies.rectangle(width * .3, height * .925, width * .025, height * .15, {
+        isStatic: true
+    });
+    walls[7] = Bodies.rectangle(width * .45, height * .925, width * .025, height * .15, {
+        isStatic: true
+    });
+    walls[8] = Bodies.rectangle(width * .6, height * .925, width * .025, height * .15, {
+        isStatic: true
+    });
+    walls[9] = Bodies.rectangle(width * .75, height * .925, width * .025, height * .15, {
+        isStatic: true
+    })
+    walls[10] = Bodies.rectangle(width * .67, height * .75, width * .025, height * .08, {
+        isStatic: true
+    })
+
+    World.add(engine.world, walls);
 
     // run the engine
     Engine.run(engine);
@@ -89,10 +130,14 @@ function draw() {
     // I could ask for everything in the world
     // var bodies = Composite.allBodies(engine.world);
 
+
+
     background(51);
-    //    noStroke()
+    noStroke()
 
-
+    textSize(16);
+    fill(50, 125, 250)
+    text('Goal!', width * .8, height * .9)
     // Ground vertices
     var vertices = barrierB.vertices;
     beginShape();
@@ -127,7 +172,7 @@ function draw() {
     endShape();
 
 
-    //An Extra Box
+    //The Player Box
     var vertices = boxC.vertices;
     beginShape();
     fill(127, 0, 0);
@@ -135,4 +180,16 @@ function draw() {
         vertex(vertices[i].x, vertices[i].y);
     }
     endShape();
+
+    //rendering the barrier walls
+    for (var ii = 0; ii < walls.length; ii++) {
+        //        console.log("i'm wokring")
+        vertices = walls[ii].vertices;
+        beginShape();
+        fill(127);
+        for (var i = 0; i < vertices.length; i++) {
+            vertex(vertices[i].x, vertices[i].y);
+        }
+        endShape();
+    }
 }
